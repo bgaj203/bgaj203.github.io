2fe0d25130b4519b3704fdccbe64aaf96f207d4d
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-01 09:48:20 +1200

    test navBar
    Made a sticky navBar and testing whether it works

---
+++ animations.js
@@ -0,0 +1,21 @@
+//Navigation bar animation starts here
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyNaveBar()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNaveBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
+
+//Navigation bar animation stops here
