b8362373d75b6f9c444ab677a1cb8886f4bd83c8
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-01 13:46:47 +1200

    image and heading
    added a profile pictre and the name

--- animations.js
+++ animations.js
@@ -1,5 +1,4 @@
 //Navigation bar animation starts here
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {stickyNaveBar()};
 
@@ -9,7 +8,8 @@ var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the navbar when you reach its 
+//scroll position. Remove "sticky" when you leave the scroll position
 function stickyNaveBar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
