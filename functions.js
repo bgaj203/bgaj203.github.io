c5f489878ec2634869eb54cc2a55f5f41edabec9
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-09 12:31:28 +1200

    add profile card
    added the profile card, icons and extra content.
    Now have the tab functionality as well

--- functions.js
+++ functions.js
@@ -1,5 +1,24 @@
-function readFile (){
-    fetch("journey.json")
-    .then(Response => Response.json())
-    .then(jsonResponse => console.log(jsonResponse));
+function main(){
+    loadAboutMe();
+}
+
+function loadAboutMe(){
+    console.log("about me")
+    document.getElementById("aboutMe").style.display= "block"
+    document.getElementById("myExperience").style.display= "none"
+    document.getElementById("lifeAway").style.display= "none"
+}
+
+function loadMyExperience(){
+    console.log("experience")
+    document.getElementById("aboutMe").style.display= "none"
+    document.getElementById("myExperience").style.display= "block"
+    document.getElementById("lifeAway").style.display= "none"
+}
+
+function loadLifeAway(){
+    console.log("life away")
+    document.getElementById("aboutMe").style.display= "none"
+    document.getElementById("myExperience").style.display= "none"
+    document.getElementById("lifeAway").style.display= "block"
 }
