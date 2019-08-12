dccbf0f402e9ed5c6c80120576da01a75dbaeeea
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-12 15:44:46 +1200

    Revert "add profile card"
    This reverts commit c5f489878ec2634869eb54cc2a55f5f41edabec9.

--- functions.js
+++ functions.js
@@ -1,24 +1,5 @@
-function main(){
-    loadAboutMe();
-}
-
-function loadAboutMe(){
-    console.log("about me")
-    document.getElementById("aboutMe").style.display= "block"
-    document.getElementById("myExperience").style.display= "none"
-    document.getElementById("lifeAway").style.display= "none"
-}
-
-function loadMyExperience(){
-    console.log("experience")
-    document.getElementById("aboutMe").style.display= "none"
-    document.getElementById("myExperience").style.display= "block"
-    document.getElementById("lifeAway").style.display= "none"
-}
-
-function loadLifeAway(){
-    console.log("life away")
-    document.getElementById("aboutMe").style.display= "none"
-    document.getElementById("myExperience").style.display= "none"
-    document.getElementById("lifeAway").style.display= "block"
+function readFile (){
+    fetch("journey.json")
+    .then(Response => Response.json())
+    .then(jsonResponse => console.log(jsonResponse));
 }
