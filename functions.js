224075b31973e413bdc91ece2407b1649042c911
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-14 06:15:50 +1200

    navbar
    fixed the sticky navbar and made a better mobile nav bar

--- functions.js
+++ functions.js
@@ -1,14 +1,23 @@
-function readFile (){
-    fetch("journey.json")
-    .then(Response => Response.json())
-    .then(jsonResponse => console.log(jsonResponse));
-}
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
 
-function mobileNavigation() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+let navbar = document.getElementById("navbar");
+
+let sticky = navbar.offsetTop;
+
+window.onscroll = function() {
+  stickyNavBar()
+};
+
+function stickyNavBar() {
+  console.log("in here")
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   }
+  else {
+    navbar.classList.remove("sticky")
+  }
+}
