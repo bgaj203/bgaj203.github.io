a05a5b27b59c23d579d42619c6ec44e3223c5073
bgaj203 <39352100+bgaj203@users.noreply.github.com>
2019-08-14 10:01:51 +1200

    created functions
    made functionality between the tabs. Now can swtich between home, about me and interests

--- functions.js
+++ functions.js
@@ -13,11 +13,32 @@ window.onscroll = function() {
 };
 
 function stickyNavBar() {
-  console.log("in here")
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   }
   else {
     navbar.classList.remove("sticky")
   }
+}
+
+function main() {
+  home()
+}
+
+function home(){
+  document.getElementById("home").style.display="flex"
+  document.getElementById("aboutMe").style.display="none"
+  document.getElementById("interests").style.display="none"
+}
+
+function aboutMe(){
+  document.getElementById("home").style.display="none"
+  document.getElementById("aboutMe").style.display="flex"
+  document.getElementById("interests").style.display="none"
+}
+
+function interests(){
+  document.getElementById("home").style.display="none"
+  document.getElementById("aboutMe").style.display="none"
+  document.getElementById("interests").style.display="flex"
 }
