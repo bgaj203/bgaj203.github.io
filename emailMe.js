cb4c89186f3d638c286b93c6a252b314474d003d
Basitha <bgaj203@aucklanduni.ac.nz>
2019-09-18 15:25:27 +1200

    email sent
    email sent successfully. But still need to work on the message format and the data sanitisation

--- emailMe.js
+++ emailMe.js
@@ -1,22 +1,27 @@
-//const formData = []
-
-// document.getElementById('emailMeForm').addEventListener('submit', (event) => {
-//   console.log('here')
-//   formData = new FormData(event.target)
-//   event.preventDefault()
-//   event.currentTarget.reset()
-//   readData()
-// })
-
-// document.querySelector('form').addEventListener('submit', (e) => {
-//   const formData = new FormData(e.target);
-//   e.preventDefault();
-// });
+let formDataJSON = {}
+"https://script.google.com/macros/s/AKfycbxq48J7WFwfEuElAEGz6t0OO5IPfRDcEDJHVTQ9Bg/exec"
 
 readData = () => {
-  console.log("hi")
-  let formData = new FormData();
-  formData = document.getElementById('emailMeForm')
-  console.log(formData.senderName.value)
+  const formData = new FormData(document.getElementById('emailMeForm'));
+  for (let pair of formData.entries()){
+    formDataJSON[pair[0]] = pair[1]
+  }
   document.getElementById('emailMeForm').reset()
+
+  if (formDataJSON['senderHoneyPot'] == ""){
+    postRequest()
+  }
+}
+
+postRequest = () => {
+  const url = "https://script.google.com/macros/s/AKfycbxq48J7WFwfEuElAEGz6t0OO5IPfRDcEDJHVTQ9Bg/exec"
+  const xhr = new XMLHttpRequest()
+  xhr.open('POST', url)
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = () => {
+    if (xhr.status == 200) {
+      console.log('email sent')
+    }
+  }
+  xhr.send(JSON.stringify(formDataJSON))
 }
